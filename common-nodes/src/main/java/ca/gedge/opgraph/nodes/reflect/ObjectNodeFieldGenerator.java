1ca75eba708e0d206fcdd27e5b3e069620748e76
Greg Hedlund <greg.hedlund@gmail.com>
2017-03-22 10:03:45 -0230

    Don't add input/output fields for properties named 'enabled'

--- common-nodes/src/main/java/ca/gedge/opgraph/nodes/reflect/ObjectNodeFieldGenerator.java
+++ common-nodes/src/main/java/ca/gedge/opgraph/nodes/reflect/ObjectNodeFieldGenerator.java
@@ -8,15 +8,15 @@ import java.util.List;
 import ca.gedge.opgraph.util.ReflectUtil;
 
 /**
- * Class used to add input/output fields based on 
+ * Class used to add input/output fields based on
  * class definitions.
  */
 public class ObjectNodeFieldGenerator {
-	
+
 	private final List<ObjectNodePropertyInputField> inputFields = new ArrayList<ObjectNodePropertyInputField>();
-	
+
 	private final List<ObjectNodePropertyOutputField> outputFields = new ArrayList<ObjectNodePropertyOutputField>();
-	
+
 	public ObjectNodeFieldGenerator() {
 		super();
 	}
@@ -28,10 +28,10 @@ public class ObjectNodeFieldGenerator {
 	public List<ObjectNodePropertyOutputField> getOutputFields() {
 		return outputFields;
 	}
-	
+
 	/**
 	 * Scan the given class and generate input/ouput fields.
-	 * 
+	 *
 	 * @param clazz
 	 */
 	public void scanClass(Class<?> clazz) {
@@ -40,39 +40,43 @@ public class ObjectNodeFieldGenerator {
 	}
 
 	/*
-	 * Scan class for get methods and return list of 
+	 * Scan class for get methods and return list of
 	 * created output fields
-	 * 
+	 *
 	 * @return outputs
 	 */
 	private List<ObjectNodePropertyOutputField> scanClassGetters(Class<?> clazz) {
 		final List<ObjectNodePropertyOutputField> retVal = new ArrayList<ObjectNodePropertyOutputField>();
 		final Method[] methods = clazz.getMethods();
-		
+
 		for(Method method:methods) {
 			String propName = null;
 			Class<?> outputType = null;
-			if(method.getName().startsWith("get") 
+			if(method.getName().startsWith("get")
 					&& method.getParameterTypes().length == 0) {
 				propName = method.getName().substring(3);
 				propName = propName.replace(propName.charAt(0), Character.toLowerCase(propName.charAt(0)));
-				
+
+				if(propName.equals("enabled")) {
+					continue;
+				}
+
 				outputType = method.getReturnType();
-				
+
 			} else if(method.getName().startsWith("is")
 					&& method.getParameterTypes().length == 0
 					&& method.getReturnType() == Boolean.class) {
 				propName = method.getName().substring(2);
 				propName = propName.replace(propName.charAt(0), Character.toLowerCase(propName.charAt(0)));
-				
+
 				outputType = method.getReturnType();
 			}
 			if(propName == null || propName.length() == 0 || propName.equals("class")) continue;
-			
+
 			if(outputType.isPrimitive()) {
 				outputType = ReflectUtil.wrapperClassForPrimitive(outputType);
 			}
-			
+
 			final ObjectNodePropertyOutputField outputField = new ObjectNodePropertyOutputField(propName, "", outputType, method);
 			outputField.setFixed(true);
 //			putField(outputField);
@@ -80,7 +84,7 @@ public class ObjectNodeFieldGenerator {
 		}
 		return retVal;
 	}
-	
+
 	private List<ObjectNodePropertyInputField> scanClassSetters(Class<?> clazz) {
 		final List<ObjectNodePropertyInputField> retVal = new ArrayList<ObjectNodePropertyInputField>();
 		final Method[] methods = clazz.getMethods();
@@ -89,12 +93,14 @@ public class ObjectNodeFieldGenerator {
 					&& method.getParameterTypes().length == 1) {
 				String propName = method.getName().substring(3);
 				propName = propName.replace(propName.charAt(0), Character.toLowerCase(propName.charAt(0)));
-				
+
+				if(propName.equals("enabled")) continue;
+
 				Class<?> inputType = method.getParameterTypes()[0];
 				if(inputType.isPrimitive()) {
 					inputType = ReflectUtil.wrapperClassForPrimitive(inputType);
 				}
-				
+
 				final ObjectNodePropertyInputField propField = new ObjectNodePropertyInputField(propName, "", inputType, method);
 				propField.setOptional(true);
 //				putField(propField);
@@ -103,5 +109,5 @@ public class ObjectNodeFieldGenerator {
 		}
 		return retVal;
 	}
-	
+
 }

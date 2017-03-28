c486fdaafe779b69e27e0801d06d9e6f38eb940f
Greg Hedlund <greg.hedlund@gmail.com>
2017-03-28 10:19:08 -0230

    FIx auto-layout of level 0 nodes

--- app/src/main/java/ca/gedge/opgraph/app/AutoLayoutManager.java
+++ app/src/main/java/ca/gedge/opgraph/app/AutoLayoutManager.java
@@ -20,25 +20,25 @@ import ca.gedge.opgraph.extensions.NodeMetadata;
 /**
  * Layout nodes in a graph in a logical manner.  Nodes
  * are laid out horizontally, with nodes traversed depth first.
- * 
+ *
  */
 public class AutoLayoutManager {
-	
+
 	private final static int DEFAULT_SPACE = 15;
-	
+
 	private CompoundEdit cmpEdit = new CompoundEdit();
-	
+
 	private int prefWidth = -1;
-	
+
 	public AutoLayoutManager() {
-		
+
 	}
-	
+
 	public void layoutGraph(OpGraph graph) {
 		// ensure correct sorting
 		graph.invalidateSort();
 		graph.topologicalSort();
-		
+
 		final List<OpNode> modifiedNodes = checkNodes(graph);
 
 		cmpEdit = new CompoundEdit();
@@ -52,17 +52,17 @@ public class AutoLayoutManager {
 			.forEach(node -> {
 				final Dimension size = placeNode(node, xRef.get(), yRef.get());
 				placedNodes.add(node);
-				maxYRef.set(yRef.get());
+				maxYRef.set(yRef.get() + (int)size.getHeight());
 				followOutputs(graph, node, xRef.get() + size.width + DEFAULT_SPACE,
 						yRef.get(), xRef.get() + size.width + DEFAULT_SPACE, maxYRef, placedNodes);
-				
+
 				yRef.set(maxYRef.get() + DEFAULT_SPACE);
 			});
 		cmpEdit.end();
-		
+
 		modifiedNodes.forEach(node -> node.putExtension(JComponent.class, null));
 	}
-	
+
 	private List<OpNode> checkNodes(OpGraph graph) {
 		final List<OpNode> modifiedNodes = new ArrayList<>();
 		for(OpNode node:graph) {
@@ -73,11 +73,11 @@ public class AutoLayoutManager {
 		}
 		return modifiedNodes;
 	}
-	
+
 	/**
-	 * Follow outputs for a node, placing each node in depth first 
+	 * Follow outputs for a node, placing each node in depth first
 	 * ordering.
-	 * 
+	 *
 	 * @param graph
 	 * @param node
 	 * @param x currentX
@@ -94,18 +94,18 @@ public class AutoLayoutManager {
 			if(!placedNodes.contains(dest)) {
 				final Dimension size = placeNode(dest, x, y);
 				placedNodes.add(dest);
-				
+
 				if(getPreferredWidth() > 0 && (x+size.width) > getPreferredWidth()) {
 					// wrap node
 					x = xoff;
 					y = maxYRef.get() + DEFAULT_SPACE;
 					placeNode(dest, x, y);
 				}
-				
+
 				maxYRef.set(Math.max(maxYRef.get(), y + size.height));
-				
+
 				int nextX = x + size.width + DEFAULT_SPACE;
-				if(lastNode != null && 
+				if(lastNode != null &&
 						(graph.getLevel(lastNode) != graph.getLevel(dest))) {
 					x = nextX;
 				}
@@ -118,19 +118,19 @@ public class AutoLayoutManager {
 			lastNode = dest;
 		}
 	}
-	
+
 	public int getPreferredWidth() {
 		return this.prefWidth;
 	}
-	
+
 	public void setPreferredWidth(int width) {
 		this.prefWidth = width;
 	}
-	
+
 	/**
 	 * Place node a given {x, y} point and return
 	 * the dimension of the node.
-	 * 
+	 *
 	 * @param node
 	 * @param x
 	 * @param y
@@ -147,15 +147,15 @@ public class AutoLayoutManager {
 			meta = new NodeMetadata(x, y);
 			node.putExtension(NodeMetadata.class, meta);
 		}
-		
+
 		final int deltaX = x - meta.getX();
 		final int deltaY = y - meta.getY();
 		if(deltaX != 0 || deltaY != 0)
 			addEdit(new MoveNodesEdit(Collections.singletonList(node), deltaX, deltaY));
-		
+
 		return retVal;
 	}
-	
+
 	private void addEdit(UndoableEdit edit) {
 		cmpEdit.addEdit(edit);
 	}
@@ -163,5 +163,5 @@ public class AutoLayoutManager {
 	public UndoableEdit getUndoableEdit() {
 		return this.cmpEdit;
 	}
-	
+
 }

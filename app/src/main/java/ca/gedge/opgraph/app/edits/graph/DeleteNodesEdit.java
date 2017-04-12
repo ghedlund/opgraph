b52b0cafe0b2a3c65945fbcc9761e8937952e50f
Greg Hedlund <greg.hedlund@gmail.com>
2017-04-12 06:43:42 -0230

    Issue warning instead of throwing an exception for recoverable issues

--- app/src/main/java/ca/gedge/opgraph/app/edits/graph/DeleteNodesEdit.java
+++ app/src/main/java/ca/gedge/opgraph/app/edits/graph/DeleteNodesEdit.java
@@ -2,17 +2,17 @@
  * Copyright (C) 2012 Jason Gedge <http://www.gedge.ca>
  *
  * This file is part of the OpGraph project.
- * 
+ *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation, either version 3 of the License, or
  * (at your option) any later version.
- * 
+ *
  * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU General Public License for more details.
- * 
+ *
  * You should have received a copy of the GNU General Public License
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
@@ -20,6 +20,7 @@ package ca.gedge.opgraph.app.edits.graph;
 
 import java.util.ArrayList;
 import java.util.Collection;
+import java.util.Collections;
 import java.util.List;
 import java.util.Set;
 import java.util.TreeSet;
@@ -55,7 +56,7 @@ public class DeleteNodesEdit extends AbstractUndoableEdit {
 	/**
 	 * Constructs a delete edit that removes a collection of nodes from a
 	 * specified canvas model.
-	 * 
+	 *
 	 * @param graph  the graph to which this edit will be applied
 	 * @param nodes  the nodes to remove
 	 */
@@ -83,6 +84,10 @@ public class DeleteNodesEdit extends AbstractUndoableEdit {
 	private void perform() {
 		for(OpNode link : nodes)
 			graph.remove(link);
+	}
+
+	public List<OpNode> getNodes() {
+		return Collections.unmodifiableList(this.nodes);
 	}
 
 	//

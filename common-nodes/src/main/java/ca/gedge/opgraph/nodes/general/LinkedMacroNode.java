10cb71c5a6738e74f3d9526917184dfa3c1648a8
Greg Hedlund <greg.hedlund@gmail.com>
2017-08-25 16:30:17 -0230

    Added LinkedMacroNode

---
+++ common-nodes/src/main/java/ca/gedge/opgraph/nodes/general/LinkedMacroNode.java
@@ -0,0 +1,91 @@
+package ca.gedge.opgraph.nodes.general;
+
+import java.io.IOException;
+import java.net.URI;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+import ca.gedge.opgraph.OpGraph;
+import ca.gedge.opgraph.io.OpGraphSerializer;
+import ca.gedge.opgraph.io.OpGraphSerializerFactory;
+
+public class LinkedMacroNode extends MacroNode {
+	
+	private static final Logger LOGGER = Logger.getLogger(LinkedMacroNode.class.getName());
+	
+	private static final Map<URI, OpGraph> graphMap = new HashMap<>();
+
+	/*
+	 * Load graph at given uri or the in-memory graph if it has already been loaded.
+	 * The last modifed time is checked and the graph is reloaded if necessary.
+	 */
+	private static OpGraph loadGraph(URI uri) throws IOException {
+		OpGraph graph = null;
+		boolean loadGraph = true;
+		if(graphMap.containsKey(uri)) {
+			graph = graphMap.get(uri);
+			// TODO check timestamp
+			loadGraph = false;
+		}
+		
+		if(loadGraph) {
+			final OpGraphSerializer serializer = OpGraphSerializerFactory.getDefaultSerializer();
+			graph = serializer.read(uri.toURL().openStream());
+			
+			// check to make sure the graph has a single MacroNode
+			if(graph.getVertices().size() != 1) {
+				throw new IOException("Graph has incorrect number of nodes");
+			}
+			
+			if(!(graph.getVertices().get(0) instanceof MacroNode)) {
+				throw new IOException("Graph must consist of a single MacroNode");
+			}
+			
+			graphMap.put(uri, graph);
+		}
+		return graph;
+	}
+	
+	private URI uri;
+	
+	public LinkedMacroNode() {
+		super();
+	}
+	
+	public LinkedMacroNode(URI uri) {
+		super();
+		
+		setUri(uri);
+	}
+	
+	public void setUri(URI uri) {
+		try {
+			final OpGraph originalGraph = LinkedMacroNode.loadGraph(uri);
+			final MacroNode node = (MacroNode)originalGraph.getVertices().get(0);
+			
+			final OpGraph graph = new OpGraph();
+			graph.add(node);
+			
+			// publish inputs/outputs from macro
+			for(PublishedInput pi:node.getPublishedInputs()) {
+				publish(pi.getKey(), node, pi);
+			}
+			
+			for(PublishedOutput po:node.getPublishedOutputs()) {
+				publish(po.getKey(), node, po);
+			}
+			setGraph(graph);
+			
+			this.uri = uri;
+		} catch (IOException e) {
+			LOGGER.log(Level.SEVERE, e.getLocalizedMessage(), e);
+		}		
+	}
+	
+	public URI getUri() {
+		return this.uri;
+	}
+	
+}
---
+++ common-nodes/src/main/java/ca/gedge/opgraph/nodes/xml/LinkedMacroNodeXMLSerializer.java
@@ -0,0 +1,122 @@
+package ca.gedge.opgraph.nodes.xml;
+
+import java.io.IOException;
+import java.lang.reflect.Constructor;
+import java.lang.reflect.InvocationTargetException;
+import java.net.URI;
+
+import javax.xml.XMLConstants;
+import javax.xml.namespace.QName;
+
+import org.w3c.dom.Document;
+import org.w3c.dom.Element;
+import org.w3c.dom.Node;
+import org.w3c.dom.NodeList;
+
+import ca.gedge.opgraph.InputField;
+import ca.gedge.opgraph.OpGraph;
+import ca.gedge.opgraph.OutputField;
+import ca.gedge.opgraph.extensions.Extendable;
+import ca.gedge.opgraph.io.xml.XMLSerializer;
+import ca.gedge.opgraph.io.xml.XMLSerializerFactory;
+import ca.gedge.opgraph.nodes.general.LinkedMacroNode;
+import ca.gedge.opgraph.nodes.general.MacroNode;
+
+public class LinkedMacroNodeXMLSerializer implements XMLSerializer {
+	static final String NAMESPACE = "http://gedge.ca/ns/opgraph-common-nodes";
+	static final String PREFIX = "ogcn";
+
+	// qualified names
+	static final QName MACRO_QNAME = new QName(NAMESPACE, "linked_macro", PREFIX);
+	
+	@Override
+	public void write(XMLSerializerFactory serializerFactory, Document doc, Element parentElem, Object obj)
+			throws IOException {
+		if(obj == null)
+			throw new IOException("Null object given to serializer");
+
+		if(!(obj instanceof LinkedMacroNode))
+			throw new IOException(LinkedMacroNodeXMLSerializer.class.getName() + " cannot write objects of type " + obj.getClass().getName());
+
+		// setup namespace for document
+		final Element rootEle = doc.getDocumentElement();
+		rootEle.setAttributeNS(XMLConstants.XMLNS_ATTRIBUTE_NS_URI,
+				XMLConstants.XMLNS_ATTRIBUTE + ":" + PREFIX, NAMESPACE);
+
+		// Create node element
+		final MacroNode macro = (MacroNode)obj;
+		final Element macroElem = doc.createElementNS(NAMESPACE, PREFIX + ":" + MACRO_QNAME.getLocalPart());
+
+		macroElem.setAttribute("id", macro.getId());
+		macroElem.setAttribute("type", obj.getClass().getName());
+
+		if(!macro.getName().equals(macro.getDefaultName()))
+			macroElem.setAttribute("name", macro.getName());
+
+		if(!macro.getDescription().equals(macro.getDefaultDescription())) {
+			final Element descriptionElem = doc.createElementNS(NAMESPACE, PREFIX + ":description");
+			descriptionElem.setTextContent(macro.getDescription());
+			macroElem.appendChild(descriptionElem);
+		}
+
+		// Macro graph uri
+		final Element uriElem = doc.createElementNS(NAMESPACE, PREFIX + ":uri");
+		uriElem.setTextContent(((LinkedMacroNode)obj).getUri().toASCIIString());
+		macroElem.appendChild(uriElem);
+
+		// Extensions last
+		if(macro.getExtensionClasses().size() > 0) {
+			final XMLSerializer serializer = serializerFactory.getHandler(Extendable.class);
+			if(serializer == null)
+				throw new IOException("No XML serializer for extensions");
+
+			serializer.write(serializerFactory, doc, macroElem, macro);
+		}
+
+		parentElem.appendChild(macroElem);
+	}
+
+	@Override
+	public Object read(XMLSerializerFactory serializerFactory, OpGraph graph, Object parent, Document doc, Element elem)
+			throws IOException {
+		LinkedMacroNode macro = null;
+		if(MACRO_QNAME.equals(XMLSerializerFactory.getQName(elem))) {
+			// Read children
+			final NodeList children = elem.getChildNodes();
+			for(int childIndex = 0; childIndex < children.getLength(); ++childIndex) {
+				final Node node = children.item(childIndex);
+				if(node instanceof Element) {
+					final Element childElem = (Element)node;
+					final QName name = XMLSerializerFactory.getQName(childElem);
+					if(name.getLocalPart().equals("uri")) {
+						final String uriTxt = childElem.getTextContent();
+						final URI uri = URI.create(uriTxt);
+						macro = new LinkedMacroNode(uri);
+					}
+				}
+			}
+
+			// Set attributes
+			if(macro != null) {
+				if(elem.hasAttribute("id"))
+					macro.setId(elem.getAttribute("id"));
+
+				if(elem.hasAttribute("name"))
+					macro.setName(elem.getAttribute("name"));
+			}
+		}
+
+		return macro;
+	}
+
+	@Override
+	public boolean handles(Class<?> cls) {
+		return (cls == LinkedMacroNode.class);
+	}
+
+	@Override
+	public boolean handles(QName name) {
+		return MACRO_QNAME.equals(name);
+	}
+
+}
